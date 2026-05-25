eea863669d66217cff7aba1d80bea31982e99731
bf24001-web <bf24001@ues.edu.sv>
2026-05-25 17:22:03 -0600

    Actualizacion model asistencia

--- src/instructores/models/asistencia_clases.rs
+++ src/instructores/models/asistencia_clases.rs
@@ -6,17 +6,19 @@ pub struct AsistenciaClase {
     pub id_asistencia: i32,
     pub id_miembro: Option<i32>,
     pub id_clase: Option<i32>,
-    pub fecha_asistencia: String,
+    pub fecha_asistencia: NaiveDate,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NuevaAsistenciaClase {
     pub id_miembro: Option<i32>,
     pub id_clase: Option<i32>,
+    pub fecha_asistencia: NaiveDate,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ActualizarAsistenciaClase {
     pub id_miembro: Option<i32>,
     pub id_clase: Option<i32>,
+    pub fecha_asistencia: NaiveDate,
 }

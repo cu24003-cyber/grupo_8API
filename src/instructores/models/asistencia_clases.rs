65efd3ec5ba2198a7c0aa85347a91b5748e73d37
bf24001-web <bf24001@ues.edu.sv>
2026-05-25 16:39:22 -0600

    model_asistencia_clases_impementacion

---
+++ src/instructores/models/asistencia_clases.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow, PartialEq)]
+pub struct AsistenciaClase {
+    pub id_asistencia: i32,
+    pub id_miembro: Option<i32>,
+    pub id_clase: Option<i32>,
+    pub fecha_asistencia: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NuevaAsistenciaClase {
+    pub id_miembro: Option<i32>,
+    pub id_clase: Option<i32>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ActualizarAsistenciaClase {
+    pub id_miembro: Option<i32>,
+    pub id_clase: Option<i32>,
+}
